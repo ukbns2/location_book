60077764f4b1a04816ea79f0eeec90facf388ae8
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2022-11-24 12:37:14 +0000

    deviseのフォームヘルパーの変更&nameのデータを受け取れるように追加設定

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -24,4 +24,21 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+   devise_parameter_sanitizer.permit(:sign_up,keys: [:name])
+  end
+
+
+
 end
