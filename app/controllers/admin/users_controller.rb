361534ac04df32b057b3b372d7b04c625bdfffc7
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2022-11-30 11:56:40 +0000

    管理者)ユーザー詳細画面の作成完了

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,6 +5,7 @@ class Admin::UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get '/' => 'homes#top'
-    resources :users, only: [:index, :create, :edit, :update]
+    resources :users, only: [:index, :show, :edit, :update]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
