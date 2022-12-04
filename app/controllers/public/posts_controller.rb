d62a8bc9fae4a6c27391f82dce0e0e3b0f671725
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2022-12-04 03:56:00 +0000

    ユーザー)投稿編集画面の作成着手

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -3,12 +3,28 @@ class Public::PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to post_path(@post.id)
+    else
+      render :new
+    end
+  end
+
   def index
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:image, :title, :body, :rate, :postal_code, :address)
   end
 end
