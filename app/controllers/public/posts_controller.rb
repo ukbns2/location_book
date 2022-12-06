ff7d13c7b7f58b068c678f28e54e37d87e4e8766
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2022-12-06 05:27:51 +0000

    ユーザー)タグ投稿機能の実装途中

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -6,7 +6,9 @@ class Public::PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
+    tag_list = params[:post][:name].split(', ')
     if @post.save
+      @post.save_tag(tag_list)
       redirect_to post_path(@post.id)
     else
       render :new
@@ -14,10 +16,13 @@ class Public::PostsController < ApplicationController
   end
 
   def index
+    @posts = Post.page(params[:page]).per(10)
+    @tag_list = Tag.all
   end
 
   def show
     @post = Post.find(params[:id])
+    @post_tags = @post.tags
   end
 
   def edit
@@ -25,6 +30,6 @@ class Public::PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:image, :title, :body, :rate, :postal_code, :address)
+    params.require(:post).permit(:image, :title, :body, :rate, :postal_code, :address, :tag_name)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,27 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :book_tags
-  has_many :tags, through: :book_tags
+  has_many :post_tags
+  has_many :tags, through: :post_tags
   has_one_attached :image
+
+  def save_tag(sent_tags)
+    # タグが存在していれば、タグの名前を配列として全て取得
+    current_tags = self.tags.pluck(:name) unless self.tags.nil?
+    # 現在取得したタグから送られてきたタグを除いてoldtagとする
+    old_tags = current_tags - sent_tags
+    # 送信されてきたタグから現在存在するタグを除いたタグをnewtagとする
+    new_tags = sent_tags - current_tags
+
+    # 古いタグを消す
+    old_tags.each do |old|
+      self.tags.delete Tags.find_by(name: old)
+    end
+
+    # 新しいタグを保存
+    new_tags.each do |new|
+      new_post_tag = Tag.find_or_create_by(name: new)
+      self.tags << new_post_tag
+    end
+  end
+
 end
