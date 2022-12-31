bbe4888efd22d17311fc3cec734b84dcd4524769
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2022-12-31 10:48:51 +0000

    管理者)投稿日の表示設定と一覧画面での表示を新規順に変更

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,12 +2,12 @@ class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
 
   def top
-    @posts = Post.page(params[:page]).per(10)
+    @posts = Post.order('id DESC').page(params[:page]).per(12)
   end
 
   private
   def post_params
-    params.require(:post).permit(:image, :title, :body, :rate, :postal_code, :address, :is_draft, :user_id, :latitude, :longitude, tag_ids: [])
+    params.require(:post).permit(:image, :title, :body, :rate, :postal_code, :address, :is_draft, :delete, :user_id, :latitude, :longitude, tag_ids: [])
   end
 
 end
