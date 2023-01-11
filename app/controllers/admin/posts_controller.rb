f6f89b7df8d28aa7e02560fc79c9d8cf01d289b0
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2023-01-11 12:42:53 +0000

    管理者)投稿詳細画面でいいね数とコメント件数及びコメントが表示されるように記述追加

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -2,6 +2,7 @@ class Admin::PostsController < ApplicationController
   before_action :authenticate_admin!
   def show
     @post = Post.find(params[:id])
+    @comments = @post.comments
   end
 
   def edit
